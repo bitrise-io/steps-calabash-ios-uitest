3ef16429f1ce8c41c53b918bb85074459872798d
Krisztián Gödrei <godreikrisztian@gmail.com>
2016-10-25 16:58:36 +0200

    go-toolkit (#6)
    * go-toolkit
    
    * step.yml update

--- step.sh
+++ step.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -ex
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+tmp_gopath_dir="$(mktemp -d)"
 
-set -e
+go_package_name="github.com/bitrise-steplib/steps-calabash-ios-uitest"
+full_package_path="${tmp_gopath_dir}/src/${go_package_name}"
+mkdir -p "${full_package_path}"
 
-export BUNDLE_GEMFILE="${this_script_dir}/Gemfile"
+rsync -avh --quiet "${THIS_SCRIPT_DIR}/" "${full_package_path}/"
 
-bundle install
-bundle exec ruby "$this_script_dir/step.rb" \
-	-b "${simulator_device}" \
-	-c "${simulator_os_version}"
+export GOPATH="${tmp_gopath_dir}"
+export GO15VENDOREXPERIMENT=1
+go run "${full_package_path}/main.go"
